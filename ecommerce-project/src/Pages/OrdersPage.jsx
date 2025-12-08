eed133d64a3a387b2042936d79be662acb66c84d
PrinceLord-Lab <princelord.mendoza@gmail.com>
2025-12-08 19:05:57 +0800

    changed all <a> to <NavLink>

--- ecommerce-project/src/Pages/OrdersPage.jsx
+++ ecommerce-project/src/Pages/OrdersPage.jsx
@@ -1,11 +1,12 @@
 import { Header } from "../components/Header";
+import { NavLink } from "react-router";
 import "./OrdersPage.css";
 
 export function OrdersPage() {
   return (
     <>
       <title>Orders</title>
-      <link rel="icon" type="image/svg+xml" href="orders-favicon.png" />
+      <link rel="icon" type="image/svg+xml" href="/images/orders-favicon.png" />
 
       <Header />
 
@@ -55,11 +56,11 @@ export function OrdersPage() {
               </div>
 
               <div className="product-actions">
-                <a href="/tracking">
+                <NavLink to="/tracking">
                   <button className="track-package-button button-secondary">
                     Track package
                   </button>
-                </a>
+                </NavLink>
               </div>
 
               <div className="product-image-container">
@@ -84,11 +85,11 @@ export function OrdersPage() {
               </div>
 
               <div className="product-actions">
-                <a href="/tracking">
+                <NavLink to="/tracking">
                   <button className="track-package-button button-secondary">
                     Track package
                   </button>
-                </a>
+                </NavLink>
               </div>
             </div>
           </div>
@@ -133,11 +134,11 @@ export function OrdersPage() {
               </div>
 
               <div className="product-actions">
-                <a href="/tracking">
+                <NavLink to="/tracking">
                   <button className="track-package-button button-secondary">
                     Track package
                   </button>
-                </a>
+                </NavLink>
               </div>
             </div>
           </div>
--- ecommerce-project/src/Pages/TrackingPage.jsx
+++ ecommerce-project/src/Pages/TrackingPage.jsx
@@ -1,15 +1,23 @@
 import { Header } from "../components/Header";
+import { NavLink } from "react-router";
 import "./TrackingPage.css";
 
 export function TrackingPage() {
   return (
     <>
+      <title>Tracking</title>
+      <link
+        rel="icon"
+        type="image/svg+xml"
+        href="/images/tracking-favicon.png"
+      />
+
       <Header />
       <div className="tracking-page">
         <div className="order-tracking">
-          <a className="back-to-orders-link link-primary" href="/orders">
+          <NavLink className="back-to-orders-link link-primary" to="/orders">
             View all orders
-          </a>
+          </NavLink>
           <div className="delivery-date">Arriving on Monday, June 13</div>
           <div className="product-info">
             Black and Gray Athletic Cotton Socks - 6 Pairs
