a23aa46dc3de64da6dfc51e63cfb50001338e86b
PrinceLord-Lab <princelord.mendoza@gmail.com>
2025-12-10 12:51:31 +0800

    updated the header cart props from nofoundpage.

--- ecommerce-project/src/Pages/NotFoundPage.jsx
+++ ecommerce-project/src/Pages/NotFoundPage.jsx
@@ -1,11 +1,11 @@
 import "./NotFoundPage.css";
 import { Header } from "../components/Header";
 
-export function NotFoundPage() {
+export function NotFoundPage({ cart }) {
   return (
     <div className="Container">
       <title>Not Found Page</title>
-      <Header />
+      <Header cart={cart} />
       <div className="error">
         <h1>Error 404</h1>
         <h2>Page Not Found</h2>
--- ecommerce-project/src/Pages/TrackingPage.jsx
+++ ecommerce-project/src/Pages/TrackingPage.jsx
@@ -4,6 +4,7 @@ import axios from "axios";
 import "./TrackingPage.css";
 import { useEffect, useState } from "react";
 import dayjs from "dayjs";
+import { DeliveryDate } from "./checkout/DeliveryDate";
 
 export function TrackingPage({ cart }) {
   const { orderId, productId } = useParams();
@@ -29,6 +30,20 @@ export function TrackingPage({ cart }) {
   const orderProduct = order.products.find((orderProduct) => {
     return orderProduct.productId === productId;
   });
+
+  const totalDeliveryTimeMs =
+    orderProduct.estimatedDeliveryTimeMs - order.orderTimeMs;
+  const timePassedMs = dayjs().valueOf() - order.orderTimeMs;
+
+  let deliveryPercent = (timePassedMs / totalDeliveryTimeMs) * 100;
+  if (deliveryPercent > 100) {
+    deliveryPercent = 100;
+  }
+
+  const isPreparing = deliveryPercent < 33;
+  const isShipped = deliveryPercent >= 33 && deliveryPercent < 100;
+  const isDelivered = deliveryPercent === 100;
+
   return (
     <>
       <title>Tracking</title>
@@ -45,18 +60,34 @@ export function TrackingPage({ cart }) {
             View all orders
           </NavLink>
           <div className="delivery-date">
-            Arriving on{" "}
+            {deliveryPercent >= 100 ? "Delivered on " : "Arriving on "}
             {dayjs(orderProduct.estimatedDeliveryTimeMs).format("dddd, MMMM D")}
           </div>
           <div className="product-info">{orderProduct.product.name}</div>
           <div className="product-info">Quantity: {orderProduct.quantity}</div>
           <img className="product-image" src={orderProduct.product.image} />
           <div className="progress-labels-container">
-            <div className="progress-label">Preparing</div>
-            <div className="progress-label current-status">Shipped</div>
-            <div className="progress-label">Delivered</div>
+            <div
+              className={`progress-label ${isPreparing && "current-status"}`}
+            >
+              Preparing
+            </div>
+            <div className={`progress-label ${isShipped && "current-status"}`}>
+              Shipped
+            </div>
+            <div
+              className={`progress-label ${isDelivered && "current-status"}`}
+            >
+              Delivered
+            </div>
           </div>
           <div className="progress-bar-container">
+            <div
+              className="progress-bar"
+              style={{
+                width: `${deliveryPercent}%`,
+              }}
+            ></div>
             <div className="progress-bar"></div>
           </div>
         </div>
