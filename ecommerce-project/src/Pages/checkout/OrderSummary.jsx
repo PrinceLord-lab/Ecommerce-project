4906e4356340723fc85534c39c66f0e5e1d48426
PrinceLord-Lab <princelord.mendoza@gmail.com>
2025-12-10 18:05:34 +0800

    create delete cart feature

--- ecommerce-project/src/Pages/checkout/OrderSummary.jsx
+++ ecommerce-project/src/Pages/checkout/OrderSummary.jsx
@@ -19,7 +19,7 @@ export function OrderSummary({ cart, deliveryOptions, loadCart }) {
               <div className="cart-item-details-grid">
                 <img className="product-image" src={cartItem.product.image} />
 
-                <CartItemDetails cartItem={cartItem} />
+                <CartItemDetails cartItem={cartItem} loadCart={loadCart} />
 
                 <DeliveryOptions
                   loadCart={loadCart}
